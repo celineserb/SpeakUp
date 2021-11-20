9e45777cc2045f4e1955eebadf4b9eaaea93b4e0
celineserb <38558430+celineserb@users.noreply.github.com>
2021-11-20 11:09:07 +0100

    stop

--- src/index.js
+++ src/index.js
@@ -1,39 +1,59 @@
 const { board } = window.miro;
 const {sticky} = './app.js';
+
+
+var stop = 0;
 async function init() {
+  var speechRecognition = new webkitSpeechRecognition();
   await board.ui.on("icon:click", async () => {
-    
-    let speechRecognition = new webkitSpeechRecognition();
+    stop=stop+1;
+    console.log(stop)
+    /*let speechRecognition = new webkitSpeechRecognition();
     let final_transcript = "";
 
     speechRecognition.lang = 'English';
 
     speechRecognition.continuous = true;
-    speechRecognition.interimResults = true;
-    speechRecognition.start();
-    console.log("testssssssssssssssssssssssssssssssssssssssssssssssssss");
-    
-    
-
-    speechRecognition.onresult = (event) => {
-      let interim_transcript = "";
-
-      for (let i = event.resultIndex; i < event.results.length; ++i) {
-        if (event.results[i].isFinal) {
-          final_transcript += event.results[i][0].transcript;
-        } else {
-          interim_transcript += event.results[i][0].transcript;
-        }
-        console.log(interim_transcript);
-      }
+    speechRecognition.interimResults = true;*/
+    if(stop%2 != 0){
       
-      if (interim_transcript=='sticky' ){
+      let final_transcript = "";
+
+      speechRecognition.lang = 'English';
+
+      speechRecognition.continuous = true;
+      speechRecognition.interimResults = true;
+      speechRecognition.start();
+      console.log("testssssssssssssssssssssssssssssssssssssssssssssssssss");
+      
+      speechRecognition.onresult = (event) => {
+        let interim_transcript = "";
+  
+        for (let i = event.resultIndex; i < event.results.length; ++i) {
+          if (event.results[i].isFinal) {
+            final_transcript += event.results[i][0].transcript;
+          } else {
+            interim_transcript += event.results[i][0].transcript;
+          }
+          console.log(interim_transcript);
+        }
         
-        board.ui.openPanel({ pageUrl: "app.html", maxHeight:7 })
+        if (interim_transcript=='sticky' ){
+          
+          board.ui.openPanel({ pageUrl: "app.html", maxHeight:7 })
+        }
+        console.log("interim", interim_transcript);
+           
       }
-      console.log("interim", interim_transcript);
-         
+
+    }else{
+      speechRecognition.stop();
+
     }
+    
+    
+
+
 
 
   })
